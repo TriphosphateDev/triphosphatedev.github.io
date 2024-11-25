4b41859a98461cd18da89cbc817441f0fa64d587
Triphosphate <triphosphatelp@gmail.com>
2024-11-25 13:43:29 -0500

    2

--- cypress/support/e2e.js
+++
@@ -1,8 +0,0 @@
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Prevent Cypress from failing on uncaught exceptions from your application
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-}) 
