e97826c275609ae956fe6e1554689175e7eb0136
GitHub Action <action@github.com>
2024-11-25 18:53:17 +0000

    Add config file for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export const config = {
   GOOGLE_SHEETS_ENDPOINT: 'AKfycbx9P_NPnYmMaOBD6AFbbCDE6H3T4fR43GhbE0gUhlsTyemjqX-ku62o8VzOVepeDjnE7Q',
-  GA_TRACKING_ID: 'G-CWBEE471XP',
-  SITE_URL: 'https://tripmixes.com'
+  GA_TRACKING_ID: 'G-CWBEE471XP'
 };
