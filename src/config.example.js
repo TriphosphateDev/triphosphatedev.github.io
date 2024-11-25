f8e74636d38ecc85c7cde425cd9c47ea394e9d30
Triphosphate <triphosphatelp@gmail.com>
2024-11-25 16:22:15 -0500

    6

--- src/config.example.js
+++ src/config.example.js
@@ -1,8 +1,4 @@
 export const config = {
-    PROXYCHECK_API_KEY: 'your-api-key-here',
-    PROXYCHECK_PUBLIC_KEY: 'your-public-key-here',
-    PROXYCHECK_API_ENDPOINT: 'https://proxycheck.io/v2',
-    RECAPTCHA_SITE_KEY: '6LewkogqAAAAANUh-L1KOeelY9mBl2I6evdrhRAr',
     GOOGLE_SHEETS_ENDPOINT: 'your-sheets-endpoint-here',
     GA_TRACKING_ID: 'G-CWBEE471XP'
 }; 
--- src/config.prod.js
+++ src/config.prod.js
@@ -1,5 +1,3 @@
 export const config = {
-    PROXYCHECK_API_ENDPOINT: 'https://proxycheck.io/v2',
-    RECAPTCHA_SITE_KEY: '6LewkogqAAAAANUh-L1KOeelY9mBl2I6evdrhRAr',
     GA_TRACKING_ID: 'G-CWBEE471XP'
 }; 
